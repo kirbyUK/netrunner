e6cd03493878ff6408d1159b9e13f3b3dc976194
Alex Kerr <kirby@cpan.org>
2024-04-28 16:28:48 +0100

    Add commandline arguments

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -1,6 +1,8 @@
+import argparse
 from datetime import date
+import functools
 from io import TextIOWrapper
-from math import ceil
+from math import floor
 from multiprocessing import Pool
 from netrunner.netrunnerdb.card import Card
 from netrunner.netrunnerdb.decklist import Decklist
@@ -14,41 +16,75 @@ from sklearn.cluster import KMeans
 
 
 def main():
+    output_file, start_date, end_date, tournament_format, top_percentage, corp_clusters, runner_clusters = args()
     abr = AlwaysBeRunning()
 
     # Get all events that match our filters.
     events = list(
         filter(
             lambda event:
-                (event.format or "") == "standard" and
-                (date(2024, 3, 18) <= (event.date or date(2000, 1, 1)) <= date.today()),
+                (event.format or "") == tournament_format and
+                (start_date <= (event.date or date(2000, 1, 1)) <= end_date),
             all_events(abr)
         )
     )
 
     # Get all decklists from these events that match our filters, split out over
     # a multiprocessing pool to get it done quicker.
+    decklist_getter = functools.partial(decklists_from_event, top_percentage)
     with Pool() as pool:
-        decklists = [x for xs in pool.map(decklists_from_event, events) for x in xs]
+        decklists = [x for xs in pool.map(decklist_getter, events) for x in xs]
 
     # Split our decklist tuples into corp and runner sets.
     corp_decks = set([decklist[0] for decklist in decklists if decklist[0] is not None])
     runner_decks = set([decklist[1] for decklist in decklists if decklist[1] is not None])
     
     # Cluster the corp decklists and print the markdown.
-    with open(f"rwr_{date.today().isoformat()}.md", "w", encoding="utf-8") as f:
+    with open(output_file, "w", encoding="utf-8") as f:
         f.write(f"## Corp\n")
-        cluster_decklists(corp_decks, 14, f)
+        cluster_decklists(corp_decks, corp_clusters, f)
         f.write(os.linesep)
 
         # Cluster the runner decklists and print the markdown.
         f.write(f"## Runner\n")
-        cluster_decklists(runner_decks, 10, f)
+        cluster_decklists(runner_decks, runner_clusters, f)
         f.write(os.linesep)
 
 
-def decklists_from_event(tournament: Event) -> List[Tuple[Optional[Decklist], Optional[Decklist]]]:
-    """Get all decklists from an event that fall in the top 30%."""
+def args() -> Tuple[str, date, date, str, float, int, int]:
+    """Parse commandline arguments."""
+    parser = argparse.ArgumentParser(
+        prog="cluster",
+        description="k-Means Clustering for Netrunner top decks"
+    )
+
+    parser.add_argument("--output", "-o", default=f"rwr_{date.today().isoformat()}.md", help="Output filename")
+    parser.add_argument("--start-date", default="2024-03-18", help="Start date for completed events (inclusive)")
+    parser.add_argument("--end-date", default=date.today().isoformat(), help="End date for completed events (inclusive)")
+    parser.add_argument("--format", default="standard", choices=["standard", "startup"], help="The format to get completed decks for")
+    parser.add_argument("--percentage", default=30, type=int, help="Percentage of decks to collect from tournaments (0-100)")
+    parser.add_argument("--corp-clusters", default=15, type=int, help="Number of clusters to use for corp decks")
+    parser.add_argument("--runner-clusters", default=10, type=int, help="Number of clusters to use for runner decks")
+
+    args = parser.parse_args()
+
+    if args.percentage < 0 or args.percentage > 100:
+        sys.stderr.write("--percentage arg must be between 0 and 100\n")
+        raise Exception
+
+    return (
+        args.output,
+        date.fromisoformat(args.start_date),
+        date.fromisoformat(args.end_date),
+        args.format,
+        args.percentage / 100,
+        args.corp_clusters,
+        args.runner_clusters
+    )
+
+
+def decklists_from_event(top_percentage: float, tournament: Event) -> List[Tuple[Optional[Decklist], Optional[Decklist]]]:
+    """Get all decklists from an event that fall in the top given percentage."""
     decks = []
 
     try:
@@ -58,7 +94,7 @@ def decklists_from_event(tournament: Event) -> List[Tuple[Optional[Decklist], Op
 
     try:
         for entry in filter(
-            lambda entry: 1 <= (entry.rank_swiss or 0) <= ceil(len(tournament.entries()) * 0.3),
+            lambda entry: 1 <= (entry.rank_swiss or 0) <= floor(len(tournament.entries()) * top_percentage),
             tournament.entries()
         ):
             corp = None
